cfa3ae54e40a1d8ddf24fb98762596180aab3bfd
Shulhan <ms@kilabit.info>
2024-09-07 17:50:18 +0700

    Release ciigo v0.13.2 (2024-09-07)
    Update on asciidoctor-go bring new features and enhancements,
    
    * Support document attribute "leveloffset".
    
      The ":leveloffset:" on document attribute allow increment or decrement
      the heading level on included files.
    
      Reference: https://docs.asciidoctor.org/asciidoc/latest/directives/include-with-leveloffset/
    
    * Use strict document header format.
    
      Previously, an empty line before Document Title cause the parser
      stop parsing the document header, now an empty lines are skipped.
      Also document attribute can be place anywhere, either before or after
      title, and in between attributes; now it can be only placed after
      revision or author or title.

--- ciigo.go
+++ ciigo.go
@@ -27,7 +27,7 @@ const (
 )
 
 // Version define the latest tagged release of this module.
-var Version = `0.13.1`
+var Version = `0.13.2`
 
 // defExcludes define default files to be excludes on GoEmbed.
 var defExcludes = []string{
