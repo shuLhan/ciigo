4c38d5303c0f2d4af9f75452989951a36652c953
Shulhan <ms@kilabit.info>
2025-02-01 09:49:49 +0700

    Release ciigo v0.15.1 (2025-02-01)
    [BUG FIX]
    Fix section detected as paragraph after list and comment.
    Previously, given the following markup,
    ----
    * Sub list
    +
    Sub list content.
    
    //}}}
    //{{{
    == Sub 2
    //}}
    ----
    The section "Sub 2" will be parsed as paragraph instead of new section.
    
    [CHORE]
    In the linter, we replace the fieldalignment and shadow using our internal
    gocheck command.
    This linters actually have an API that can be combined into a program,
    which provided by package "pakakeh.go/lib/goanalysis".

--- ciigo.go
+++ ciigo.go
@@ -28,7 +28,7 @@ const (
 )
 
 // Version define the latest tagged release of this module.
-var Version = `0.15.0`
+var Version = `0.15.1`
 
 // defExcludes define default files to be excludes on GoEmbed.
 var defExcludes = []string{
