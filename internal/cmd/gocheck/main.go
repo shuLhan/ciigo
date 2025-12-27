9ac6c7af20c0e456a1551eca751221878fcc4a9e
Shulhan <ms@kilabit.info>
2025-12-27 13:36:25 +0700

    all: update missing licenses and copyright information

--- internal/cmd/gocheck/main.go
+++ internal/cmd/gocheck/main.go
@@ -1,6 +1,5 @@
 // SPDX-FileCopyrightText: 2024 M. Shulhan <ms@kilabit.info>
-//
-// SPDX-License-Identifier: BSD-3-Clause
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 // Program gocheck implement go static analysis using [Analyzer] that are not
 // included in the default go vet.
